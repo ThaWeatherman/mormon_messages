d5b993eeb7270619a15fc899803942ec4fd33e7c
Sean Beck <seanmckaybeck@gmail.com>
2013-10-27 21:51:45 -0600

    Finished product

--- bot.py
+++ bot.py
@@ -1,37 +1,75 @@
 import requests
 import tweepy
+from bs4 import BeautifulSoup
+import time
+import datetime
 
 class Bot:
     def __init__(self):
-        get_auth_tokens()
+        self.valid_links = ['www.lds.org/media-library/video','www.lds.org/pages/mormon-messages','www.lds.org/youth/video']
+        self.get_auth_tokens()
+    def run(self):
+        while True:
+            self.download_page()
+            self.extract_links()
+            self.timeout()
     def timeout(self):
-        import time
-        # sleep for 30 minutes
-        time.sleep(1800)
-    def check_diff(self):
+        # sleep for 60 minutes
+        time.sleep(3600)
+    def extract_links(self):
+        soup = BeautifulSoup(open('page.html'))
+        tags = soup.find_all('a')
+        links = []
+        for link in tags:
+            links.append(link.get('href'))
+        valid = []
+        for link in links:
+            for possible in self.valid_links:
+                if possible in link:
+                    valid.append(link)
+                    break
+        no_dups = list(set(valid))
+        self.compare_new_and_old(no_dups)
+    def compare_new_and_old(self,valid):
+        lines = []
+        with open('existing.txt','rb') as f:
+            lines = f.readlines()
+        stripped = []
+        if len(lines) > 0:
+            stripped = [line.strip() for line in lines]
+        new_links = []
+        for link in valid:
+            if link not in stripped:
+                new_links.append(link)
+        if len(new_links) > 0:
+            for link in new_links:
+                self.tweet_new(link)
+            with open('existing.txt','a') as f:
+                for link in new_links:
+                    f.write(link+'\n')
     def get_auth_tokens(self):
-        f = open('authenticators.txt', 'rb')
-        self.consumer_key = f.readline().strip()
-        self.consumer_secret = f.readline().strip()
-        self.access_token = f.readline().strip()
-        self.access_secret = f.readline().strip()
-        f.close()
+        with open('authenticators.txt', 'rb') as f:
+            self.consumer_key = f.readline().strip()
+            self.consumer_secret = f.readline().strip()
+            self.access_token = f.readline().strip()
+            self.access_secret = f.readline().strip()
         auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
         auth.set_access_token(self.access_token, self.access_secret)
         self.auth = auth
         api = tweepy.API(self.auth)
         self.api = api
-    def tweet_new(self, link, msg_name):
-        tweet = 'Updated: ' + msg_name + ' ' + link
+    def tweet_new(self, link):
+        tweet = 'New Mormon Message at ' + link + ' ' + datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y")
         self.api.update_status(tweet)
     def download_page(self):
         page = 'https://www.lds.org/pages/mormon-messages/topics?lang=eng'
         r = requests.get(page, stream=True)
         while r.status_code != 200:
             r = requests.get(page, stream=True)
-        f = open('updated.html', 'wb')
-        f.write(r.content)
-        f.close()
+        with open('page.html', 'wb') as f:
+            for chunk in r.iter_content(10000):
+                f.write(chunk)
 
 if __name__ == "__main__":
     b = Bot()
+    b.run()
